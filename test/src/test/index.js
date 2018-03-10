6fbe7df735e79018df2d43bf7603b25699fd22c8
Ib Green <ib@uber.com>
2018-03-10 15:23:46 -0800

    Add BrowserTestDriver class

--- test/src/test/index.js
+++ test/src/test/index.js
@@ -1,2 +1,3 @@
 import './make-spy.spec';
 import './browser-driver.spec';
+import './browser-test-driver.spec';
--- test/src/test/make-spy.spec.js
+++ test/src/test/make-spy.spec.js
@@ -1,6 +1,6 @@
 import test from 'tape-catch';
 
-import {makeSpy} from 'probe.gl/test';
+import {makeSpy} from 'probe.gl/test-utils';
 
 test('import "probe.gl/test"', t => {
   t.ok(typeof makeSpy, 'makeSpy symbol imported');
