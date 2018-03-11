4095bf8eb857307d1355f59aac4f06ac47a7496f
Ib Green <ib@uber.com>
2018-03-11 07:23:32 -0700

    Improve top-level exports

--- bench.js
+++ bench.js
@@ -1,1 +1,3 @@
+// This file enables: import 'probe.gl/bench'.
+// Note: Must be published using package.json "files" field
 module.exports = require('./dist/bench');
--- test.js
+++ test.js
@@ -1,2 +1,4 @@
-// DEPRECATED, use test-utils
+// This file enables: import 'probe.gl/test'.
+// Note: Must be published using package.json "files" field
+console.warn("import 'probe.gl/test' is deprecated, use import 'probe.gl/test-utils'"); // eslint-disable-line
 module.exports = require('./dist/test-utils');
