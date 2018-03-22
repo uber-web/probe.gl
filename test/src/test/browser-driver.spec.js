01b7db480a4534635146a98fa84685fc23e36cc9
Ib Green <ib@uber.com>
2018-03-22 09:27:06 -0700

    Add spy.returns

--- test/src/test/browser-driver.spec.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape-catch';
-
-import {BrowserDriver} from 'probe.gl/test-utils';
-
-test('Top-level imports', t => {
-  t.ok(BrowserDriver, 'BrowserDriver symbol imported');
-  t.end();
-});
--- test/src/test/index.js
+++
@@ -1,3 +0,0 @@
-import './make-spy.spec';
-import './browser-driver.spec';
-import './browser-test-driver.spec';
