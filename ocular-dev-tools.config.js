cb94a97cee9fd5bca4e44e9c4681287d707afa27
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-05-08 18:36:24 -0700

    Clean up dev setup (#72)

---
+++ ocular-dev-tools.config.js
@@ -0,0 +1,16 @@
+const config = {
+  lint: {
+    paths: ['modules', 'examples', 'test'],
+    extensions: ['js']
+  },
+
+  entry: {
+    test: 'test/index.js',
+    'test-browser': 'test/browser.js',
+    bench: 'test/bench/index.js',
+    'bench-browser': 'test/bench/browser.js',
+    size: 'test/size/import-nothing.js'
+  }
+};
+
+module.exports = config;
