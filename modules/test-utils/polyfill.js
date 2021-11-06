fcf3b59f29818257fcf94b5de21e2d12d162ff03
Ib Green <ib@unfolded.ai>
2021-11-05 17:17:09 -0700

    chore: Add typescript build

--- modules/test-utils/polyfill.js
+++ modules/test-utils/polyfill.js
@@ -1,17 +1,14 @@
 // Polyfill for non-puppeteer environments
 // so that test bundle can run in other browsers
-
 // A puppeteer injected method returns a promise
 function noOp() {
   return Promise.resolve();
 }
-
 function emulateInput() {
   // eslint-disable-next-line
   console.error('BrowserTestDriver: emulateInput is not available');
   return Promise.resolve();
 }
-
 function captureAndDiffScreen() {
   // eslint-disable-next-line
   console.error('BrowserTestDriver: emulateInput is not available');
@@ -22,10 +19,14 @@ function captureAndDiffScreen() {
     success: true
   });
 }
-
+// @ts-expect-error
 if (!window.browserTestDriver_finish) {
+  // @ts-expect-error
   window.browserTestDriver_fail = noOp;
+  // @ts-expect-error
   window.browserTestDriver_finish = noOp;
+  // @ts-expect-error
   window.browserTestDriver_emulateInput = emulateInput;
+  // @ts-expect-error
   window.browserTestDriver_captureAndDiffScreen = captureAndDiffScreen;
 }
