df8ef9782cb462d69780c7cc8b2d2d7cafae8e0d
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2023-01-01 14:34:05 -0800

    docs: Whats New, Upgrade Guide, Changelog

--- modules/test-utils/test/browser-test-driver.spec.js
+++ modules/test-utils/test/browser-test-driver.spec.js
@@ -35,6 +35,7 @@ test('BrowserTestDriver#import', t => {
 });
 
 test('BrowserTestDriver#ImageDiff', async t => {
+  // @ts-expect-error
   if (typeof document === 'undefined' || !window.browserTestDriver_captureAndDiffScreen) {
     t.comment('ImageDiff only works in automated browser tests');
     t.end();
@@ -51,6 +52,7 @@ test('BrowserTestDriver#ImageDiff', async t => {
   };
 
   try {
+    // @ts-expect-error
     let result = await window.browserTestDriver_captureAndDiffScreen(diffSettings);
     if (result.success) {
       t.pass(`Screenshot matches golden image: ${result.matchPercentage}`);
@@ -61,11 +63,9 @@ test('BrowserTestDriver#ImageDiff', async t => {
     }
 
     const ctx = canvas.getContext('2d');
-    if (!ctx) {
-      throw new Error('context');
-    }
     ctx.fillStyle = '#ff0';
     ctx.fillRect(10, 10, 12, 12);
+    // @ts-expect-error
     result = await window.browserTestDriver_captureAndDiffScreen(diffSettings);
     if (result.success) {
       t.fail(`Screenshot should not match golden image: ${result.matchPercentage}`);
