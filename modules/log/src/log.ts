2e1e2135843cbdd089cd2954a7af333b412e1ca0
Ib Green <ib@unfolded.ai>
2023-01-02 11:30:21 -0500

    chore(bench): Enable all typescript checks on `bench` module (#220)

--- modules/log/src/log.ts
+++ modules/log/src/log.ts
@@ -400,7 +400,7 @@ export function normalizeArguments(opts: {
   logLevel;
   message;
   collapsed?: boolean;
-  args?: IArguments;
+  args?: IArguments | undefined;
   opts?;
 }): {
   logLevel: number;
