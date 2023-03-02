ae8e32ccb4b54ca185d63968ee6e7210df1f3503
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2023-03-02 14:24:33 -0800

    Fix globals bundle error (#229)

--- modules/env/src/lib/globals.ts
+++ modules/env/src/lib/globals.ts
@@ -1,10 +1,19 @@
-export const global = globalThis;
+// Do not name these variables the same as the global objects - will break bundling
+const global_ = globalThis;
 // eslint-disable-next-line consistent-this
-export const self = globalThis.self || globalThis.window || globalThis.global;
-export const window = (globalThis.window ||
-  globalThis.self ||
-  globalThis.global) as unknown as Window;
-export const document = globalThis.document || ({} as Document);
-export const process = globalThis.process || {};
-export const console = globalThis.console;
-export const navigator = globalThis.navigator || ({} as Navigator);
+const self_ = globalThis.self || globalThis.window || globalThis.global;
+const window_ = (globalThis.window || globalThis.self || globalThis.global) as unknown as Window;
+const document_ = globalThis.document || ({} as Document);
+const process_ = globalThis.process || {};
+const console_ = globalThis.console;
+const navigator_ = globalThis.navigator || ({} as Navigator);
+
+export {
+  global_ as global,
+  self_ as self,
+  window_ as window,
+  document_ as document,
+  process_ as process,
+  console_ as console,
+  navigator_ as navigator
+};
