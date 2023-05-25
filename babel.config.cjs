78b289f0e96f40b5d8f91a9213e75c86076f6f44
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2023-05-25 10:21:09 -0700

    Update babel config (#237)

--- babel.config.cjs
+++ babel.config.cjs
@@ -7,6 +7,13 @@ module.exports = getBabelConfig({
     ignore: [
       // babel can't process .d.ts
       /\.d\.ts$/
-    ]
+    ],
+    // These settings reduce the verbosity of transpile outputs
+    assumptions: {
+      // When declaring classes, assume that methods don't shadow getters on the superclass and that the program doesn't depend on methods being non-enumerable.
+      setClassMethods: true,
+      // When using public class fields, assume that they don't shadow any getter in the current class, in its subclasses or in its superclass.
+      setPublicClassFields: true
+    }
   }
 });
